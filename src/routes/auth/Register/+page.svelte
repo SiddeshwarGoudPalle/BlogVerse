54f2b864d372eea71b2fbf702a42f1f6fb57879a
Poojitha Bhukya <bhukyapoojitha@gmail.com>
2024-08-01 12:53:09 +0530

    added register and login

--- src/routes/auth/Register/+page.svelte
+++ src/routes/auth/Register/+page.svelte
@@ -65,6 +65,13 @@
         />
       </div>
 
+      <div class="flex justify-center">
+        <button
+          type="submit"
+          class=" py-2 px-4 bg-blue-600 text-white font-semibold rounded-md shadow-md hover:bg-blue-700 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 transition-transform transform hover:scale-105"
+        >
+          Register
+        </button>
       </div>
     </form>
   </div>
